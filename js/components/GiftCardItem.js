be10c7eca759b051b69c1a9235d4758433a058d2
Your Name <aaadntjr@naver.com>
2025-12-07 15:08:43 +0900

    Product Card element

--- js/components/GiftCardItem.js
+++ js/components/GiftCardItem.js
@@ -2,7 +2,6 @@ class GiftCardItem extends HTMLElement {
   constructor() {
     super();
 
-    // 1. Create the component's internal structure directly in the light DOM
     this.innerHTML = `
       <div class="gift__image">
         <img src="" alt="">
@@ -14,52 +13,52 @@ class GiftCardItem extends HTMLElement {
       </div>
     `;
 
-    // 2. Cache element references for performance
-    this.imageEl = this.querySelector('.gift__image img');
-    this.soldoutEl = this.querySelector('.soldout');
-    this.termsEl = this.querySelector('.gift__info-terms');
-    this.descEl = this.querySelector('.gift__info-desc');
+    this.imageEl = this.querySelector(".gift__image img");
+    this.soldoutEl = this.querySelector(".soldout");
+    this.termsEl = this.querySelector(".gift__info-terms");
+    this.descEl = this.querySelector(".gift__info-desc");
   }
 
-  // 3. Define which attributes to watch for changes
   static get observedAttributes() {
-    return ["image-src", "image-alt", "terms", "description", "sold-out", "terms-color"];
+    return [
+      "image-src",
+      "image-alt",
+      "terms",
+      "description",
+      "sold-out",
+      "terms-color",
+    ];
   }
 
-  // 4. Handle attribute changes by directly manipulating the DOM
   attributeChangedCallback(name, oldValue, newValue) {
     if (oldValue === newValue) return;
 
     switch (name) {
-      case 'image-src':
+      case "image-src":
         this.imageEl.src = newValue;
         break;
-      case 'image-alt':
+      case "image-alt":
         this.imageEl.alt = newValue;
         break;
-      case 'terms':
+      case "terms":
         this.termsEl.textContent = newValue;
         break;
-      case 'description':
+      case "description":
         this.descEl.textContent = newValue;
         break;
-      case 'terms-color':
-        // Apply color directly as an inline style
+      case "terms-color":
         this.termsEl.style.color = newValue;
         break;
-      case 'sold-out':
-        // Toggle display style based on the attribute's string value
-        this.soldoutEl.style.display = newValue === 'true' ? 'flex' : 'none';
+      case "sold-out":
+        this.soldoutEl.style.display = newValue === "true" ? "flex" : "none";
         break;
     }
   }
 
   connectedCallback() {
-    // Set the host element's class for external styling
-    this.classList.add('gift__card-item');
+    this.classList.add("gift__card-item");
 
-    // Apply initial attributes that might have been set before the element was upgraded
-    this.constructor.observedAttributes.forEach(attr => {
+    this.constructor.observedAttributes.forEach((attr) => {
       if (this.hasAttribute(attr)) {
         this.attributeChangedCallback(attr, null, this.getAttribute(attr));
       }
@@ -67,4 +66,4 @@ class GiftCardItem extends HTMLElement {
   }
 }
 
-customElements.define('gift-card-item', GiftCardItem);
+customElements.define("gift-card-item", GiftCardItem);
